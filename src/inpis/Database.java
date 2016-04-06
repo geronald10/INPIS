24f363f55ff08b0af8b841a209e32ff8315ab24e
geronald10 <geronald10@gmail.com>
2016-04-07 04:47:33 +0700

    create database connection to project

---
+++ src/inpis/Database.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inpis;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Goronald
+ */
+public class Database {
+    
+    private static Connection connection;
+    
+    public static Connection getConnection() {
+        
+        if(connection == null) {
+            try {
+                String url = "jdbc:mysql://localhost/inpis";
+                DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+                connection = (com.mysql.jdbc.Connection) DriverManager.getConnection(url, "root", "");
+                System.out.println("koneksi berhasil");
+            }catch(SQLException e){
+                System.out.println("koneksi gagal\n" + e);
+            }
+        }
+        return connection;
+    }
+    
+//    public static void main(String[] args) {
+//        getConnection();
+//    }
+}
